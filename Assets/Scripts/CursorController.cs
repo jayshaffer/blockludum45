90dadff8f3e71bb5f7f5a6b1dd5b616ecf3700d1
Jay Shaffer <shaffersan@gmail.com>
2019-10-04 23:53:10 -0600

    Init

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public float speed;
+    bool canAdjust = true;
+    Rigidbody2D _rb;
+    Vector3 direction;
+    Vector3 origin;
+    Vector3 lastMovePos;
+    void Start()
+    {
+       _rb = GetComponent<Rigidbody2D>(); 
+       origin = transform.position;
+       lastMovePos = new Vector3(-100, -100, 0);
+    }
+
+    void Update()
+    {
+       if(Input.GetButton("Left") && canAdjust){
+           TriggerMove(new Vector3(-1, 0, 0) * speed);
+       }
+       if(Input.GetButton("Up") && canAdjust){
+           TriggerMove(new Vector3(0, 1, 0) * speed);
+       }
+       if(Input.GetButton("Down") && canAdjust){
+           TriggerMove(new Vector3(0, -1, 0) * speed);
+       }
+       if(Input.GetButton("Right") && canAdjust){
+           TriggerMove(new Vector3(1, 0, 0) * speed);
+       }
+    }
+
+    void TriggerMove(Vector3 velocity){
+        direction = velocity;
+        canAdjust = false;
+        lastMovePos = transform.position;
+    }
+
+    void FixedUpdate(){
+        Debug.Log(transform.position.x - lastMovePos.x );
+       if(Vector3.Distance(transform.position, lastMovePos) >= 1){
+           Debug.Log("Adjust Allowed");
+           canAdjust = true;
+       }
+       _rb.velocity = direction * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int gridSize;
+    public GameObject gridBox;
+
+    void Awake()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        for(int i = 0; i < gridSize; i++){
+            for(int j = 0; j < gridSize; j++){
+                Instantiate(gridBox, new Vector3(x, y, 0), Quaternion.identity);
+                x++;
+            }
+            y--;
+            x = transform.position.x;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.DrawSphere(transform.position, 1);
+    }
+}
